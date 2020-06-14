24e2c70fc019382923b788a47cc83a38605f4998
liudm <13641928647@139.com>
2020-06-14 16:13:38 +0800

    更新1

--- gitwork/src/main/java/com/atguigu/git/Gitfile.java
+++ gitwork/src/main/java/com/atguigu/git/Gitfile.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class Gitfile {
     public static void main(String[] args) {
         System.out.println("GIT file");
+        System.out.println("GIT file--update");
     }
 }
