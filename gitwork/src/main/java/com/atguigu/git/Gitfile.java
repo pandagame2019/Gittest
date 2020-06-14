9511d6d3da6be841f2659d2692bb9c8031a609f0
liudm <13641928647@139.com>
2020-06-14 16:21:52 +0800

    更新dev文件

--- gitwork/src/main/java/com/atguigu/git/Gitfile.java
+++ gitwork/src/main/java/com/atguigu/git/Gitfile.java
@@ -8,5 +8,6 @@ public class Gitfile {
     public static void main(String[] args) {
         System.out.println("GIT file");
         System.out.println("GIT file--update");
+        System.out.println("GIT file--update-dev");
     }
 }
